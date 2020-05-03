d15187e536cd9f63581f9d6f7b67177e55ba0416
Crowdbotics <team@crowdbotics.com>
2020-05-03 07:39:47 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/halicia_x_discrete_16557/MainActivity.java
@@ -0,0 +1,15 @@
+package com.halicia_x_discrete_16557;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "halicia_x_discrete_16557";
+    }
+}
